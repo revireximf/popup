ec416be3808a42191624a27dde2547394b1cc2f5
revireximf <36724276+revireximf@users.noreply.github.com>
2018-05-02 20:57:40 +0700

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,1 @@
+<center>Order via <b>SMS/WhatsApp: 08123 456 789</b> atau <b>pin BBM : ABC1234</b> dengan format:<br/><br/>    <span style='font-size:18px;font-weight:bold;border: dashed #333 1px;padding: 1px;color: #000;background:#fff;'>Nama/nama-produk/jumlah/alamat/kota</span><br/></center><br/>
